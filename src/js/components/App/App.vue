db97556bfd905e08848e365897649ffd2212cb18
Serg <64j@mail.ru>
2024-07-10 23:17:40 +0300

    up

--- src/js/components/App/App.vue
+++ src/js/components/App/App.vue
@@ -26,6 +26,7 @@ export default {
   components: { Notifications, RouterView, Tooltip, Datepicker, GlobalMenu, GlobalTabs, Component, Search },
   data () {
     return {
+      layout: null,
       loaded: false,
       sidebarWidth: this.$store.getters.get('Storage.sidebarWidth', 325)
     }
@@ -98,6 +99,7 @@ export default {
         axios.post('/bootstrap').then(({ data: response }) => {
           if (response.data) {
             this.loaded = true
+            this.layout = response.layout
 
             if (response.data.routes) {
               const routes = router.getRoutes()
@@ -134,14 +136,6 @@ export default {
             if (!this.$.appContext.components.RouterView) {
               this.$.appContext.app.use(router)
             }
-
-            const stack = {}
-
-            this.initLayout(response.layout, stack)
-
-            for (const i in stack) {
-              this.$.slots[i] = () => stack[i]
-            }
           } else {
             this.login()
           }
@@ -158,6 +152,7 @@ export default {
       }
 
       this.loaded = true
+      this.layout = false
       this.$.slots = {}
       router.to('/login')
     },
@@ -227,195 +222,6 @@ export default {
         input.classList.add('focus')
         this.$store.dispatch('set', { treeSelect: true })
       }
-    },
-    initLayout (layout, stack) {
-      layout = toRaw(layout || this.setLayoutData())
-
-      if (!layout) {
-        return
-      }
-
-      if (layout.component) {
-        if (layout.slot) {
-          if (!stack[layout.slot]) {
-            stack[layout.slot] = []
-          }
-
-          stack[layout.slot].push(this.createComponent(layout))
-          return
-        }
-
-        return this.createComponent(layout, stack)
-      }
-
-      if (typeof layout === 'string') {
-        layout = layout.replace(/href=/g, 'to=').
-            replace(/<a(.*?)>/g, '<router-link$1>').
-            replace(/(<\/a>)/g, '</router-link>')
-        return h(compile(layout))
-      }
-
-      if (typeof layout === 'object') {
-        let obj
-        if (Array.isArray(layout)) {
-          obj = []
-          layout.forEach(i => obj.push(this.initLayout(i, stack)))
-        } else {
-          obj = {}
-          for (let i in layout) {
-            const slots = this.initLayout(layout[i], stack)
-            obj[i] = () => slots
-          }
-        }
-
-        return obj
-      }
-
-      return layout
-    },
-    setLayoutData (data) {
-      data = data || toRaw(this.layout)
-
-      for (let i in data) {
-        if (typeof data[i]?.data === 'string') {
-          const keys = data[i].data.split('.')
-
-          if (!data[i].attrs) {
-            data[i].attrs = {}
-          }
-
-          data[i].attrs.data = this.findValue(keys, this.$props)
-          data[i].attrs.onAction = this.action
-        } else if (data[i]?.slots) {
-          data[i].slots = this.setLayoutData(data[i].slots)
-        } else if (Array.isArray(data[i])) {
-          data[i] = this.setLayoutData(data[i])
-        }
-      }
-
-      return data
-    },
-    createComponent (data, stack) {
-      let component
-
-      try {
-        component = this.$.appContext.components[data.component] ||
-            (data.component && (new Function('return ' + data.component + '')).call(this))
-      } catch (exception) {
-        return
-      }
-
-      if (!component) {
-        return
-      }
-
-      const slots = data.slots && this.initLayout(data.slots, stack)
-      const attrs = toRaw(data.attrs || {})
-
-      attrs.key = data.model || data.component.name || ''
-      attrs.modelValue = data?.attrs?.value
-
-      if (attrs.key === 'data') {
-        if (!attrs.data) {
-          attrs.data = this.data
-        } else {
-          attrs.modelValue = this.data
-        }
-      } else if (this.data?.[attrs.key] !== undefined) {
-        attrs.modelValue = this.data[attrs.key]
-      } else if (~attrs.key.indexOf('.')) {
-        attrs.modelValue = this.findValue(attrs.key.split('.'), this.$props)
-      }
-
-      if ((component?.extends?.props || component?.props)?.['meta']) {
-        attrs.meta = attrs.meta ?? this.meta
-      }
-
-      if ((component?.extends?.props || component?.props)?.['error']) {
-        attrs.error = this.errors?.[attrs.key]
-      }
-
-      (component.extends?.emits ?? component.emits ?? []).forEach(emit => {
-        if (emit === 'action') {
-          attrs['onAction'] = this.action
-        } else if (emit === 'update:props') {
-          attrs['onUpdate:props'] = (args) => this.updateProps(__attrs, args)
-        } else if (emit === 'update:modelValue') {
-          attrs['onUpdate:modelValue'] = this.updateModelValue
-        }
-      })
-
-      const __attrs = shallowReactive(attrs)
-
-      return h(component, __attrs, slots)
-    },
-    setDataValue (keys, value, data, first) {
-      const key = keys[0]
-
-      if (!first && data && data[key] === undefined) {
-        data[key] = {}
-      }
-
-      if (data[key] !== undefined) {
-        if (keys[1] !== undefined) {
-          keys.shift()
-          this.setDataValue(keys, value, data[key])
-        } else {
-          data[key] = value
-        }
-      }
-    },
-    findValue (keys, data) {
-      const key = keys[0]
-      let value
-
-      if (data[key] !== undefined) {
-        if (keys[1] !== undefined) {
-          keys.shift()
-          value = this.findValue(keys, data[key])
-        } else {
-          value = data[key]
-        }
-      }
-
-      return value
-    },
-    setValue (key, value) {
-      if (~key.indexOf('.')) {
-        if (this.data[key] !== undefined) {
-          this.data[key] = value
-        } else {
-          this.setDataValue(key.split('.'), value, this.$props, true)
-        }
-      } else {
-        this.data[key] = value
-      }
-    },
-    updateModelValue (value, instance) {
-      if (!instance) {
-        return
-      }
-
-      const key = typeof instance === 'object' ? instance._.vnode.key : instance
-
-      this.setValue(key, value)
-
-      if (instance?.relation?.['key']) {
-        const empty = !(Array.isArray(value) ? value.length : (!isNaN(parseFloat(value))
-            && !isNaN(value - 0)) ? parseFloat(value) : value)
-
-        this.setValue(instance.relation['key'],
-            empty ? instance.relation['falseValue'] : instance.relation['trueValue'])
-
-        if (empty && instance.relation['notEmpty']) {
-          this.$nextTick(() => this.setValue(key, instance.trueValue))
-        }
-      }
-
-      this.$emit('update:modelValue', ...arguments)
-    },
-    updateProps (oldValue, newValue) {
-      Object.assign(oldValue, newValue)
     }
   }
 }
@@ -424,31 +230,22 @@ export default {
 <template>
   <div v-if="loaded" class="app"
        :class="{ 'app-sidebar-hidden': !store.getters.get('Storage.root.sidebarShow', true) }">
-    <template v-if="Object.values($slots).length">
+    <template v-if="layout">
       <div id="app-slot-top" class="grow-0 shrink-0">
-        <slot name="top"/>
+        <global-menu :data="this.layout.find(i => i.slot === 'top').attrs.data" @action="action"/>
       </div>
       <div class="grow flex flex-row overflow-hidden relative">
-        <div id="app-slot-left" class="grow-0 shrink-0 border-r dark:border-r-black/30">
-          <slot name="left"/>
-        </div>
         <div class="grow flex flex-row overflow-hidden">
           <div id="app-slot-sidebar" class="grow-0 shrink-0 flex-col app-sidebar" :style="{ width: `${sidebarWidth}px` }">
-            <slot name="sidebar"/>
+            <Component :layout="this.layout.find(i => i.slot === 'sidebar')" @action="action"/>
           </div>
           <div class="app-resizer grow-0 shrink-0 flex" @mousedown="splitterDown">
             <div/>
           </div>
           <div id="app-slot-main" class="grow flex flex-col overflow-hidden">
-            <slot name="main"/>
+            <global-tabs @action="action"/>
           </div>
         </div>
-        <div id="app-slot-right" class="grow-0 shrink-0 border-l dark:border-l-black/30">
-          <slot name="right"/>
-        </div>
-      </div>
-      <div id="app-slot-bottom" class="grow-0 shrink-0 border-t dark:border-t-black/30">
-        <slot name="bottom"/>
       </div>
 
       <search ref="search"/>
