d917b6167b619a66f723c1f4ca1a9605528b8b97
Serg <64j@mail.ru>
2024-07-29 15:00:50 +0300

    up

--- src/js/components/Main/Main.vue
+++ src/js/components/Main/Main.vue
@@ -8,6 +8,19 @@ export default {
   methods: {
     action () {
       this.$emit('action', ...arguments)
+    },
+    onMousedownResizer (event) {
+      this.$refs.resizer.addEventListener('mousemove', this.onMousemoveResizer)
+      this.$refs.resizer.addEventListener('mouseup', this.onMouseupResizer)
+      this.$refs.resizer.x = event.clientX - this.$refs.resizer.parentElement.offsetWidth
+    },
+    onMousemoveResizer (event) {
+      let w = Math.max(Math.min(event.clientX - this.$refs.resizer.x, this.$el.offsetWidth / 2), 50)
+      this.$refs.resizer.parentElement.style.width = w + 'px'
+    },
+    onMouseupResizer (event) {
+      this.$refs.resizer.removeEventListener('mousemove', this.onMousemoveResizer)
+      this.$refs.resizer.removeEventListener('mouseup', this.onMouseupResizer)
     }
   }
 }
@@ -24,8 +37,15 @@ export default {
       <slot name="title"/>
     </div>
 
-    <div v-if="$slots['tabs']" class="app-main__tabs">
-      <slot name="tabs"/>
+    <div class="flex grow">
+      <div v-if="$slots['sidebar']" class="app-main__sidebar">
+        <div class="app-main__resizer" ref="resizer" @mousedown="onMousedownResizer"/>
+        <slot name="sidebar"/>
+      </div>
+
+      <div v-if="$slots['main']" class="app-main__main">
+        <slot name="main"/>
+      </div>
     </div>
 
     <div v-if="$slots['crumbs']" class="app-main__crumbs">
