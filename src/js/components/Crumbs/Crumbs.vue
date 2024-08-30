d9cfa2ebbf1a58ca5c1b2083296d9e5831ad6c0b
Serg <64j@mail.ru>
2024-08-30 17:33:41 +0300

    up

--- src/js/components/Crumbs/Crumbs.vue
+++ src/js/components/Crumbs/Crumbs.vue
@@ -9,30 +9,31 @@ export default {
     data: Array
   },
   setup (props) {
-    const slots = []
-
-    props.data.forEach(i => {
-      if (i.to) {
-        slots.push(h(RouterLink, {
-          class: 'app-crumbs__item truncate max-w-48 hover:opacity-80',
-          title: i.title,
-          to: i.to,
-          'data-tooltip': i.tooltip,
-          innerHTML: i.name ?? i.title
-        }))
-      } else {
-        slots.push(h('div', {
-          class: 'app-crumbs__item truncate max-w-48 hover:opacity-80',
-          title: i.title,
-          'data-tooltip': i.tooltip,
-          innerHTML: i.name ?? i.title
-        }))
-      }
-      slots.push(h('i',
-          { class: 'fa fa-angle-right opacity-60 leading-[0] mx-4 last-of-type:hidden', style: { fontSize: '70%' } }))
-    })
-
-    return () => h('div', { class: 'app-crumbs flex items-center px-2 py-1 text-sm' }, slots)
+    return () => h('div',
+        {
+          class: 'app-crumbs flex items-center px-2 py-1 text-sm'
+        },
+        props.data.map(i => [
+              i.to ? h(RouterLink, {
+                class: 'app-crumbs__item truncate max-w-48 hover:opacity-80',
+                title: i.title,
+                to: i.to,
+                'data-tooltip': i.tooltip,
+                innerHTML: i.name ?? i.title
+              }) : h('div', {
+                class: 'app-crumbs__item truncate max-w-48 hover:opacity-80',
+                title: i.title,
+                'data-tooltip': i.tooltip,
+                innerHTML: i.name ?? i.title
+              }),
+              h('i',
+                  {
+                    class: 'fa fa-angle-right opacity-60 leading-[0] mx-4 last-of-type:hidden',
+                    style: { fontSize: '70%' }
+                  }
+              )
+            ]
+        ))
   }
 }
 </script>
