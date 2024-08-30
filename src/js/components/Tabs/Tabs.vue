98d5cf017da3ce8f1ddf0dd3167941608d295281
Serg <64j@mail.ru>
2024-08-30 08:34:53 +0300

    up

--- src/js/components/Tabs/Tabs.vue
+++ src/js/components/Tabs/Tabs.vue
@@ -2,7 +2,7 @@
 import('./Tabs.css')
 import router from '../../router'
 import store from '../../store'
-import { h, onMounted, reactive, ref, renderSlot, watch } from 'vue'
+import { h, reactive, ref, renderSlot, watch } from 'vue'
 
 export default {
   __isStatic: true,
@@ -34,16 +34,14 @@ export default {
   setup (props, { emit, slots }) {
     const keyStorage = `tabs.` + props.id.toLowerCase()
     const refRows = ref()
-    const refPrev = ref()
-    const refNext = ref()
     const data = reactive({
-      active: store.getters.get(`Session.${keyStorage}`, null)
+      index: props.data.findIndex(i => i.id === store.getters.get(`Session.${keyStorage}`, null))
     })
 
     if (props.history) {
       if (typeof props.history === 'string') {
-        data.active = router.currentRoute.value.params[props.history]
-        props.data.forEach(i => i.active = i.render = i.id === data.active)
+        data.index = props.data.findIndex(i => i.id === router.currentRoute.value.params[props.history])
+        props.data.forEach(i => i.active = i.render = i.id === data.index)
 
         watch(
             () => router.currentRoute.value.params[props.history],
@@ -52,82 +50,36 @@ export default {
                 return
               }
 
-              if (data.active !== a) {
-                data.active = a
+              const index = props.data.findIndex(i => i.id === a)
+
+              if (data.index !== index) {
+                data.index = index
               }
             }
         )
       } else {
-        data.active = null
 
-        props.data.forEach(i => {
+        props.data.forEach((i, k) => {
           if (!i.route) {
             return
           }
 
           if (router.parse(i.route)['path'] === router.currentRoute.value.path) {
-            data.active = i.id
+            data.index = k
           }
         })
       }
-    } else if (!props.data.some(i => i.id === data.active) && props.data[0]) {
-      data.active = props.data[0].id
+    } else if (!props.data.some((i, k) => k === data.index) && props.data[0]) {
+      data.index = 0
       props.data[0].active = props.data[0].render = true
     } else {
-      props.data.forEach(i => i.active = i.render = i.id === data.active)
-    }
-
-    const init = (index) => {
-      let right = 0,
-          width = 0
-
-      if (refRows.value) {
-        refRows.value.styles = getComputedStyle(refRows.value)
-
-        refRows.value.querySelectorAll('.app-tabs__tab').forEach((t, i) => {
-          t.styles = getComputedStyle(t)
-
-          if (i <= index) {
-            width += t.offsetWidth + parseFloat(t.styles.marginLeft) + parseFloat(t.styles.marginRight)
-
-            if (i < index) {
-              right += t.offsetWidth + parseFloat(t.styles.marginLeft)
-            }
-          }
-        })
-
-        if (refRows.value.scrollLeft > right) {
-          refRows.value.scrollLeft = right
-        }
-
-        if (refRows.value.offsetWidth < width) {
-          refRows.value.scrollLeft = width - refRows.value.offsetWidth
-        }
-      }
-
-      if (refPrev.value) {
-        if (index) {
-          refPrev.value.classList.remove('app-tabs__disabled')
-        } else {
-          refPrev.value.classList.add('app-tabs__disabled')
-        }
-      }
-
-      if (refNext.value) {
-        if (props.data[index + 1]) {
-          refNext.value.classList.remove('app-tabs__disabled')
-        } else {
-          refNext.value.classList.add('app-tabs__disabled')
-        }
-      }
+      props.data.forEach((i, k) => i.active = i.render = k === data.index)
     }
 
     const select = (tab, index) => {
-      init(index)
+      props.data.forEach((i, k) => i.active = i.render = k === index)
 
-      props.data.forEach(i => i.active = i.render = i.id === tab.id)
-
-      data.active = tab.id
+      data.index = index
 
       store.dispatch('set', { [`Session.${keyStorage}`]: tab.id })
 
@@ -152,99 +104,123 @@ export default {
       }
     }
 
-    onMounted(() => init(props.data.findIndex(i => i.id === data.active)))
-
-    return () => {
-      return h('div',
-          {
-            id: props.id + 'Tabs',
-            class: [
-              'app-tabs',
-              props.vertical ? 'app-tabs__vertical' : '',
-              props.smallTabs ? 'app-tabs-small' : 'app-tabs-large',
-              props.navigation ? 'app-tabs__with-navigation' : '',
-              props.data.length === 1 ? 'app-tabs__without-rows' : ''
-            ]
-          },
-          [
-            props.data.length > 1 && h('div',
-                {
-                  class: 'app-tabs__rows'
-                },
-                [
-                  h('div',
-                      {
-                        class: 'app-tabs__row',
-                        ref: refRows
-                      },
-                      props.data.map((i, k) => h('div',
-                              {
-                                class: [
-                                  'app-tabs__tab',
-                                  i.id === data.active ? 'app-tabs__tab-active' : ''
-                                ],
-                                'data-tooltip': i.title,
-                                onMousedown: () => select(i, k)
-                              },
-                              [
-                                i.icon ? h('i', { class: ['app-tabs__tab-icon', i.icon] }) : null,
-                                i.name ? h('span', i.name) : null
-                              ]
-                          )
-                      )
-                  ),
-                  props.navigation && [
-                    h('i', {
-                      ref: refPrev,
-                      class: 'fa fa-angle-left app-tabs__prev disabled',
-                      onMousedown () {
-                        const index = props.data.findIndex(i => i.id === data.active) - 1
-
-                        if (props.data[index]) {
-                          select(props.data[index], index)
-                        }
-                      }
-                    }),
-                    h('i', {
-                      ref: refNext,
-                      class: 'fa fa-angle-right app-tabs__next disabled',
-                      onMousedown () {
-                        const index = props.data.findIndex(i => i.id === data.active) + 1
-
-                        if (props.data[index]) {
-                          select(props.data[index], index)
-                        }
-                      }
-                    })
-                  ]
-                ].filter(i => i)
-            ),
-            ...props.data.map(i => {
-              if (props.history || (i['needUpdate'] && i.id === data.active) || !i['needUpdate']) {
-                return h('div',
+    return () => h('div',
+        {
+          id: props.id + 'Tabs',
+          class: [
+            'app-tabs',
+            props.vertical ? 'app-tabs__vertical' : '',
+            props.smallTabs ? 'app-tabs-small' : 'app-tabs-large',
+            props.navigation ? 'app-tabs__with-navigation' : '',
+            props.data.length === 1 ? 'app-tabs__without-rows' : ''
+          ]
+        },
+        [
+          props.data.length > 1 && h('div',
+              {
+                class: 'app-tabs__rows'
+              },
+              [
+                h('div',
                     {
-                      id: props.id + `Tab-` + i.id,
-                      class: [
-                        'app-tabs__page',
-                        i.class
-                      ],
-                      style: i.id !== data.active ? { display: 'none' } : void 0
-                    },
-                    slots[i.id] && renderSlot(slots, i.id, i) || h('div', {
-                      class: 'grow flex items-center justify-center w-full h-full'
-                    }, [
-                      h('i',
-                          {
-                            class: 'inline-block rounded-full border-2 border-slate-200 border-r-slate-500 dark:border-white/20 dark:border-r-white h-5 w-5 animate-spin'
+                      class: 'app-tabs__row',
+                      ref: refRows,
+                      onVnodeUpdated (ctx) {
+                        let right = 0,
+                            width = 0
+
+                        ctx.el.querySelectorAll('.app-tabs__tab').forEach((t, i) => {
+                          t.styles = getComputedStyle(t)
+
+                          if (i <= data.index) {
+                            width += t.offsetWidth + parseFloat(t.styles.marginLeft) + parseFloat(t.styles.marginRight)
+
+                            if (i < data.index) {
+                              right += t.offsetWidth + parseFloat(t.styles.marginLeft)
+                            }
                           }
-                      )
-                    ])
-                )
-              }
-            })
-          ].filter(i => i)
-      )
-    }
+                        })
+
+                        if (ctx.el.scrollLeft > right) {
+                          ctx.el.scrollLeft = right
+                        }
+
+                        if (ctx.el.offsetWidth < width) {
+                          ctx.el.scrollLeft = width - refRows.value.offsetWidth
+                        }
+                      }
+                    },
+                    props.data.map((i, k) => h('div',
+                            {
+                              class: [
+                                'app-tabs__tab',
+                                k === data.index ? 'app-tabs__tab-active' : ''
+                              ],
+                              'data-tooltip': i.title,
+                              onMousedown: () => select(i, k)
+                            },
+                            [
+                              i.icon ? h('i', { class: ['app-tabs__tab-icon', i.icon] }) : null,
+                              i.name ? h('span', i.name) : null
+                            ]
+                        )
+                    )
+                ),
+                props.navigation && [
+                  h('i', {
+                    class: 'fa fa-angle-left app-tabs__prev disabled',
+                    onVnodeUpdated (ctx) {
+                      ctx.el.classList.toggle('app-tabs__disabled', !data.index)
+                    },
+                    onMousedown () {
+                      const index = data.index - 1
+
+                      if (props.data[index]) {
+                        select(props.data[index], index)
+                      }
+                    }
+                  }),
+                  h('i', {
+                    class: 'fa fa-angle-right app-tabs__next disabled',
+                    onVnodeUpdated (ctx) {
+                      ctx.el.classList.toggle('app-tabs__disabled', !props.data[data.index + 1])
+                    },
+                    onMousedown () {
+                      const index = data.index + 1
+
+                      if (props.data[index]) {
+                        select(props.data[index], index)
+                      }
+                    }
+                  })
+                ]
+              ].filter(i => i)
+          ),
+          ...props.data.map((i, k) => {
+            if (props.history || (i['needUpdate'] && k === data.index) || !i['needUpdate']) {
+              return h('div',
+                  {
+                    id: props.id + `Tab-` + i.id,
+                    class: [
+                      'app-tabs__page',
+                      i.class
+                    ],
+                    style: k !== data.index ? { display: 'none' } : void 0
+                  },
+                  slots[i.id] && renderSlot(slots, i.id, i) || h('div', {
+                    class: 'grow flex items-center justify-center w-full h-full'
+                  }, [
+                    h('i',
+                        {
+                          class: 'inline-block rounded-full border-2 border-slate-200 border-r-slate-500 dark:border-white/20 dark:border-r-white h-5 w-5 animate-spin'
+                        }
+                    )
+                  ])
+              )
+            }
+          })
+        ].filter(i => i)
+    )
   }
 }
 </script>
