38626b4cc1f85808bef686783b2e2fe61ec72b2e
Serg <64j@mail.ru>
2024-12-09 14:31:01 +0300

    up

--- src/js/components/Tabs/Tabs.vue
+++ src/js/components/Tabs/Tabs.vue
@@ -3,8 +3,6 @@ import TabsNavigation from './TabsNavigation.vue'
 import { computed, defineOptions, defineProps } from 'vue'
 import store from '../../store'
 
-import('./Tabs.css')
-
 defineOptions({
   __isStatic: true,
   name: 'Tabs'
@@ -56,14 +54,16 @@ const action = (...args) => {
 </script>
 
 <template>
-  <div :id="id + 'Tabs'" :class="['app-tabs', classes]">
+  <div :id="id + 'Tabs'" :class="classes"
+       class="app-tabs flex flex-col grow content-start overflow-hidden">
     <TabsNavigation v-if="navigation" v-bind="props" @action="action"/>
 
     <template v-for="(i, k) in data">
       <div v-if="history || (i['needUpdate'] && k === index) || !i['needUpdate']"
            v-show="k === index"
            :id="id + `Tab-` + i.id"
-           :class="['app-tabs__page', i.class]">
+           :class="i.class"
+           class="app-tabs__page relative flex flex-wrap items-start content-start rounded overflow-auto grow bg-white dark:bg-gray-700">
         <slot v-if="slots[i.id]" :name="i.id"/>
         <div v-else class="grow flex items-center justify-center w-full h-full">
           <i class="inline-block rounded-full border-2 border-slate-200 border-r-slate-500 dark:border-white/20 dark:border-r-white h-5 w-5 animate-spin"/>
@@ -72,3 +72,18 @@ const action = (...args) => {
     </template>
   </div>
 </template>
+
+<style scoped>
+.app-tabs__vertical {
+  @apply flex-row p-0 h-auto self-stretch overflow-visible
+}
+.app-tabs__vertical > .app-tabs__page {
+  @apply flex-col h-full !p-0 shadow-none rounded-none overflow-auto flex-nowrap
+}
+.app-tabs.app-tabs-small .app-tabs__page {
+  @apply m-0 p-0 bg-inherit
+}
+.app-tabs.app-tabs-small.app-tabs__vertical .app-tabs__rows ~ .app-tabs__page {
+  @apply !p-0
+}
+</style>
