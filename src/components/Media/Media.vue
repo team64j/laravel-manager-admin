0cec4b4222a9a6ceb4dab865a11f2736b8ad4ddc
Serg <64j@mail.ru>
2025-10-17 11:51:32 +0300

    up

--- src/components/Media/Media.vue
+++ src/components/Media/Media.vue
@@ -5,14 +5,19 @@ defineOptions({
 })
 
 const props = defineProps({
-  data: Object,
-  modelValue: [String, Object]
+  label: String,
+  data: Array, Object,
+  help: String
 })
 </script>
 
 <template>
   <div class="border">
-    <img v-if="~props.data['type'].indexOf('image/')" :src="props.data['url']" alt=""
+    <div v-if="label" class="block font-bold mb-1">
+      {{ label }}
+      <i v-if="help" class="ml-2 font-normal" :data-tooltip="help"/>
+    </div>
+    <img v-if="~data['type'].indexOf('image/')" :src="data['url']" alt=""
          class="w-auto h-auto max-w-full max-h-full">
   </div>
 </template>
